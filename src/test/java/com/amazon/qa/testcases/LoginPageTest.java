22a4abc5b55dd5b4953d4d4c4659a9c2ef412996
Abul Khayer <anikahmed@Abuls-MBP-2.home>
2020-07-02 17:41:07 -0400

    2nd commit

--- src/test/java/com/amazon/qa/testcases/LoginPageTest.java
+++ src/test/java/com/amazon/qa/testcases/LoginPageTest.java
@@ -1,5 +1,15 @@
 package com.amazon.qa.testcases;
 
+import org.testng.annotations.Test;
+
 public class LoginPageTest {
+	
+	@Test
+	public void test2() {
+		System.out.println("This is test2");
+		
+	}
+	
+	
 
 }
